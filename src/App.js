0a1bdcd701ebf5c5f13b9f170a0e390348d2c747
Lukas Pietzschmann <lukas.pietzschmann@outlook.de>
2022-09-22 21:30:06 +0200

    Added cute cat :^)

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import React, {useState, useEffect} from 'react';
 import ProjectCard from './ProjectCard';
 import avatar from './images/avatar.png';
+import cat from './images/cat.png';
+import cat_eye from './images/cat_eye.png';
 import './App.css';
 
 const projects = [
@@ -30,6 +32,31 @@ function App() {
 		let all_repos = [];
 		projects.forEach(project => all_repos.push(getRepoData(project)));
 		Promise.all(all_repos).then(repos => setRepos(repos));
+	}, []);
+
+	const angle = (cx, cy, ex, ey) => {
+		const dy = ey - cy;
+		const dx = ex - cx;
+		const rad = Math.atan2(dy, dx);
+		const deg = (rad * 180) / Math.PI;
+		return deg;
+	};
+
+	useEffect(() => {
+		const mouse_moved = ({clientX, clientY}) => {
+			const cat = document.getElementById('cat');
+			const rect = cat.getBoundingClientRect();
+			const cat_x = rect.left + rect.width / 2;
+			const cat_y = rect.top + rect.height / 2;
+			const angle_degree = angle(clientX, clientY, cat_x, cat_y);
+			const eyes = document.querySelectorAll('#eye');
+			eyes.forEach(eye => (eye.style.transform = `rotate(${90 + angle_degree}deg)`));
+		};
+
+		window.addEventListener('mousemove', mouse_moved);
+		return () => {
+			window.removeEventListener('mousemove', mouse_moved);
+		};
 	}, []);
 
 	return (
@@ -87,6 +114,43 @@ function App() {
 					LinkedIn
 				</a>
 			</footer>
+			<div>
+				<img
+					style={{
+						position: 'fixed',
+						bottom: -20,
+						left: 20
+					}}
+					id="cat"
+					src={cat}
+					width="100"
+					height="auto"
+				/>
+				<div>
+					<img
+						style={{
+							position: 'fixed',
+							bottom: 45,
+							left: 76
+						}}
+						src={cat_eye}
+						id="eye"
+						width="18"
+						height="auto"
+					/>
+					<img
+						style={{
+							position: 'fixed',
+							bottom: 44,
+							left: 41
+						}}
+						src={cat_eye}
+						id="eye"
+						width="18"
+						height="auto"
+					/>
+				</div>
+			</div>
 		</>
 	);
 }
