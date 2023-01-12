183804bceddf4abbf1e93bd2e0c8e6fd3c49e4e4
Lukas Pietzschmann <lukas.pietzschmann@outlook.de>
2023-01-12 22:01:29 +0100

    Made footer global

--- src/App.js
+++ src/App.js
@@ -152,17 +152,6 @@ function App() {
 			</div>
 			<div style={{margin: '50px 0px'}} />
 			<footer>
-				<a href="mailto:lukas.pietzschmann@outlook.de" target="_blank">
-					Email
-				</a>
-				<div style={{margin: '0px 15px'}}>•</div>
-				<a href="https://github.com/LukasPietzschmann" target="_blank">
-					GitHub
-				</a>
-				<div style={{margin: '0px 15px'}}>•</div>
-				<a href="https://www.linkedin.com/in/lukas-p-83aa261a3" target="_blank">
-					LinkedIn
-				</a>
 			</footer>
 			{isMobile ? (
 				''
--- src/index.js
+++ src/index.js
@@ -7,5 +7,18 @@ const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
 	<React.StrictMode>
 		<App />
+		<footer>
+			<a href="mailto:lukas.pietzschmann@outlook.de" target="_blank">
+				Email
+			</a>
+			<div style={{margin: '0px 15px'}}>•</div>
+			<a href="https://github.com/LukasPietzschmann" target="_blank">
+				GitHub
+			</a>
+			<div style={{margin: '0px 15px'}}>•</div>
+			<a href="https://www.linkedin.com/in/lukas-p-83aa261a3" target="_blank">
+				LinkedIn
+			</a>
+		</footer>
 	</React.StrictMode>
 );
