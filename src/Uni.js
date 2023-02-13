d20c59ca11bbd57cb032ec179bcfd421215b3568
Lukas Pietzschmann <lukas.pietzschmann@outlook.de>
2023-02-13 23:43:52 +0100

    Added and changed stuff on the uni subsite

--- src/Uni.js
+++ src/Uni.js
@@ -3,42 +3,54 @@ import './Uni.css';
 import RN from './data/rn.pdf';
 import Exercise from './data/exercise.pdf';
 import Lecture from './data/lecture.pdf';
-import CheatSheet from './data/cheatsheet.pdf';
+import Report from './data/report.pdf';
+import BA from './data/ba.pdf';
+import PA from './data/pa.pdf';
 
 function Uni() {
 	const data = [
 		{
-			thing: 'Term paper',
-			desc: 'For the course "computer networks" I wrote a short paper about HTTP and how it works. This is only a 10 page summary of everything so don\'t expect anything to advanced.',
-			links: [{name: 'Paper', link: RN}]
+			thing: 'Spark Paper',
+			desc: 'After the course "distributed computing platforms in practice" I wrote a small report summarizing the most important concepts of Apache Spark.',
+			links: [{name: 'Report', link: Report}]
+		},
+		{
+			thing: 'Spark Lecture',
+			desc: 'For the course "distributed computing platforms in practice" I held a lecture about Apache Spark and in memory processing.\
+					I also provided a short exercise sheet.',
+			links: [
+				{name: 'Slides', link: Lecture},
+				{name: 'Exercise', link: Exercise}
+			]
+		},
+		{
+			thing: 'Bachelor thesis',
+			desc: 'My bachelor thesis extended the MOSTflexiPL project. This time I implemented a compiler-backend that compiled MOSTflexiPl code into machine code using LLVM.',
+			links: [
+				{name: 'Thesis', link: BA},
+				{name: 'Code', link: 'https://github.com/LukasPietzschmann/Bachelorarbeit'}
+			]
 		},
 		{
 			thing: 'Project paper',
 			desc: 'The project paper I wrote was all about the MOSTflexiPL compilation process. I developed a TUI that lets you view the compilers internal state during the whole process.',
-			links: [{name: 'Code and paper', link: 'https://github.com/LukasPietzschmann/Projektarbeit'}]
-		},
-		{
-			thing: 'Bachelor thesis',
-			desc: 'My bachelor thesis also extended the MOSTflexiPL project. This time I implemented a compiler-backend that compiled MOSTflexiPl code into machine code using LLVM.',
-			links: [{name: 'Code and thesis', link: 'https://github.com/LukasPietzschmann/Bachelorarbeit'}]
-		},
-		{
-			thing: 'In-Memory Processing Lecture',
-			desc: 'For the course "distributed computing platforms in practice" I held a lecture about in-memory processing and how Apache Spark implements this paradigm.\
-					I also provided a short exercise sheet that covers the most important concepts.',
 			links: [
-				{name: 'Lecture', link: Lecture},
-				{name: 'Exercise', link: Exercise},
-				{name: 'Cheat-Sheet', link: CheatSheet}
+				{name: 'Paper', link: PA},
+				{name: 'Code', link: 'https://github.com/LukasPietzschmann/Projektarbeit'}
 			]
+		},
+		{
+			thing: 'Term paper',
+			desc: 'For the course "computer networks" I wrote a short paper about HTTP and how it works. This is only a 10 page summary of everything so don\'t expect anything to advanced.',
+			links: [{name: 'Paper', link: RN}]
 		}
 	];
 	return (
 		<>
 			<header>Stuff I made at University</header>
 			<p>
-				This is a little collection of things I wrote or made for various uni-courses. But unfortunately they are
-				all in german.
+				This is a little collection of things I wrote or made for various uni-courses, sorted by date
+				(descending). But unfortunately most of them are in german.
 			</p>
 			<table style={{marginTop: '2rem'}}>
 				<thead>
