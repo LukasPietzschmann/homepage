b797da988f4882c4f75ef88ab509b99e55f6c869
Lukas Pietzschmann <lukas.pietzschmann@outlook.de>
2023-12-18 16:35:19 +0100

    Updated privacy policy

--- src/Privacy.js
+++ src/Privacy.js
@@ -3,23 +3,7 @@ function Privacy() {
 		<>
 			<h1>Privacy Policy</h1>
 			<h2>Tracking</h2>
-			This Website does not make use of cookies or tracking software.
-			<h2>Hosting</h2>
-			<p>
-				This Website gets served through &quot;GithHub Pages&quot;. GitHub declares its handling of data in this{' '}
-				<a href="https://help.github.com/articles/github-privacy-statement/">Privacy Policy</a>.
-			</p>
-			<blockquote>
-				<p>
-					[...] we only collect the minimum amount of personal information that is necessary to fulfill the
-					purpose of your interaction with us [...]
-				</p>
-			</blockquote>
-			<p>
-				As a company based in the US, GitHub complies with the{' '}
-				<a href="https://www.privacyshield.gov/participant?id=a2zt000000001K2AAI">Privacy Shield</a>{' '}
-				requirements for users from the EU.
-			</p>{' '}
+			This Website does not make use of cookies or tracking or analytics software.
 		</>
 	);
 }
