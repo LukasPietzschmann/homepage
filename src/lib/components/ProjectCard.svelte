394f8a15b79b0a4e2fc80c3a2e12e05d3f52d379
Lukas Pietzschmann <lukas.pietzschmann@outlook.de>
2024-01-02 17:09:50 +0100

    Moved to fontawesome icons

--- src/lib/components/ProjectCard.svelte
+++ src/lib/components/ProjectCard.svelte
@@ -1,6 +1,13 @@
 <script lang="ts">
     import languages from "$lib/colors.json"
     import Card from "$lib/components/Card.svelte";
+    import {FontAwesomeIcon} from '@fortawesome/svelte-fontawesome';
+    import {faBookmark, faStar} from "@fortawesome/free-regular-svg-icons";
+    import {faCodeFork} from "@fortawesome/free-solid-svg-icons";
+    import {config} from '@fortawesome/fontawesome-svg-core'
+    import '@fortawesome/fontawesome-svg-core/styles.css';
+
+    config.autoAddCss = false
 
     export let urlTail: string;
     export let description: string;
@@ -15,34 +22,19 @@
         align-items: center;
         margin-right: 1rem;
     }
-
-    .move-me {
-        margin-right: 4px;
-    }
 </style>
 
 <Card>
     <div style="display: flex; align-items: center">
-        <svg class="move-me"
-             style="fill: var(--text)"
-             height=".8em"
-             width=".8em"
-             version="1.1"
-             viewBox="0 0 16 16"
-        >
-            <path
-                    fill-rule="evenodd"
-                    d="M2 2.5A2.5 2.5 0 014.5 0h8.75a.75.75 0 01.75.75v12.5a.75.75 0 01-.75.75h-2.5a.75.75 0 110-1.5h1.75v-2h-8a1 1 0 00-.714 1.7.75.75 0 01-1.072 1.05A2.495 2.495 0 012 11.5v-9zm10.5-1V9h-8c-.356 0-.694.074-1 .208V2.5a1 1 0 011-1h8zM5 12.25v3.25a.25.25 0 00.4.2l1.45-1.087a.25.25 0 01.3 0L8.6 15.7a.25.25 0 00.4-.2v-3.25a.25.25 0 00-.25-.25h-3.5a.25.25 0 00-.25.25z"
-            ></path>
-        </svg>
-        <a target="_blank" href="https://github.com/{urlTail}">{urlTail}</a>
+        <FontAwesomeIcon style="margin-right: 8px" icon={faBookmark}/>
+        <a target="_blank" rel="noopener noreferrer" href="https://github.com/{urlTail}">{urlTail}</a>
     </div>
     {description}
     <hr style="color: var(--highlight)"/>
     <div style="display: flex">
         <div class="info-block">
-          <span class="move-me"
-                style="
+          <span style="
+              margin-right: 4px;
               background-color: {languages[language]};
               width: 0.6em;
               height: 0.6em;
@@ -53,31 +45,11 @@
             {language}
         </div>
         <div class="info-block">
-            <svg class="move-me"
-                 style="fill: var(--text)"
-                 version="1.1"
-                 width="16"
-                 viewBox="0 0 16 16"
-            >
-                <path
-                        fill-rule="evenodd"
-                        d="M8 .25a.75.75 0 01.673.418l1.882 3.815 4.21.612a.75.75 0 01.416 1.279l-3.046 2.97.719 4.192a.75.75 0 01-1.088.791L8 12.347l-3.766 1.98a.75.75 0 01-1.088-.79l.72-4.194L.818 6.374a.75.75 0 01.416-1.28l4.21-.611L7.327.668A.75.75 0 018 .25zm0 2.445L6.615 5.5a.75.75 0 01-.564.41l-3.097.45 2.24 2.184a.75.75 0 01.216.664l-.528 3.084 2.769-1.456a.75.75 0 01.698 0l2.77 1.456-.53-3.084a.75.75 0 01.216-.664l2.24-2.183-3.096-.45a.75.75 0 01-.564-.41L8 2.694v.001z"
-                ></path>
-            </svg>
+            <FontAwesomeIcon style="margin-right: 4px" icon={faStar}/>
             {stars}
         </div>
         <div class="info-block">
-            <svg class="move-me"
-                 style="fill: var(--text)"
-                 version="1.1"
-                 width="16"
-                 viewBox="0 0 16 16"
-            >
-                <path
-                        fill-rule="evenodd"
-                        d="M5 3.25a.75.75 0 11-1.5 0 .75.75 0 011.5 0zm0 2.122a2.25 2.25 0 10-1.5 0v.878A2.25 2.25 0 005.75 8.5h1.5v2.128a2.251 2.251 0 101.5 0V8.5h1.5a2.25 2.25 0 002.25-2.25v-.878a2.25 2.25 0 10-1.5 0v.878a.75.75 0 01-.75.75h-4.5A.75.75 0 015 6.25v-.878zm3.75 7.378a.75.75 0 11-1.5 0 .75.75 0 011.5 0zm3-8.75a.75.75 0 100-1.5.75.75 0 000 1.5z"
-                ></path>
-            </svg>
+            <FontAwesomeIcon style="margin-right: 4px" icon={faCodeFork}/>
             {forks}
         </div>
     </div>
