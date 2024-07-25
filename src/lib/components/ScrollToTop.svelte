e7b7ade2aab5cfde8a1096868423074d1451507f
Lukas Pietzschmann <lukas.pietzschmann@outlook.de>
2024-07-25 14:35:10 +0200

    Added umami event to ScrollToTop button

--- src/lib/components/ScrollToTop.svelte
+++ src/lib/components/ScrollToTop.svelte
@@ -26,7 +26,7 @@
 <svelte:window bind:scrollY/>
 
 {#if showGotoTop}
-	<button on:mousedown={() => window.scrollTo({top: 0, behavior: "smooth"})} class="gotoTop" transition:blur>
+	<button on:mousedown={() => window.scrollTo({top: 0, behavior: "smooth"})} class="gotoTop" transition:blur data-umami-event="Scroll to top">
 		<svg xmlns="http://www.w3.org/2000/svg" width="48" height="48" viewBox="0 0 24 24">
 			<g fill="none" stroke="var(--text)" stroke-width="1.5">
 				<path fill="var(--background)" d="M2 12c0-4.714 0-7.071 1.464-8.536C4.93 2 7.286 2 12 2c4.714 0 7.071 0 8.535 1.464C22 4.93 22 7.286 22 12c0 4.714 0 7.071-1.465 8.535C19.072 22 16.714 22 12 22s-7.071 0-8.536-1.465C2 19.072 2 16.714 2 12Z"/>
