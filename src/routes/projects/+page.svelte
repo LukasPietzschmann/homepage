789b52c97e94228e856aecca87e74cacbd689e26
Lukas Pietzschmann <lukas.pietzschmann@outlook.de>
2024-10-21 18:31:16 +0200

    Changed personal projects subsite

--- src/routes/projects/+page.svelte
+++ src/routes/projects/+page.svelte
@@ -6,8 +6,10 @@
 	const personalProjects: ProjectId[] = [
 		{owner: 'LukasPietzschmann', repo: 'awesome-beamer'},
 		{owner: 'LukasPietzschmann', repo: 'telescope-tabs'},
+		{owner: 'LukasPietzschmann', repo: 'zigchr'},
 		{owner: 'LukasPietzschmann', repo: 'Haskellator9000'},
-		{owner: 'LukasPietzschmann', repo: 'LZip'}
+		{owner: 'LukasPietzschmann', repo: 'LZip'},
+		{owner: 'LukasPietzschmann', repo: 'PL0-Compiler'}
 	];
 </script>
 
@@ -19,16 +21,18 @@
 <h1>Personal Projects</h1>
 
 <p>
-	Outside of my studies, I like to implement fun little things,
-	hack on some LaTeX code, or dump way too many hours
-	into my NeoVim config. For example, last year I
-	reimplemented GZip decompression in good old C. I also
-	open-source most of my personal work, so check out my
-	<a data-umami-event="GitHub" href="https://github.com/LukasPietzschmann">GitHub</a>!
-	Some of my projects even attracted contributions from
-	others, which I’m way too excited about 😆.
+	Outside of my <a data-umami-event="Studies" href="/uni">studies</a>, I like to
+	implement fun little things, hack on some LaTeX code, or dump way too many hours
+	into my <a data-umami-event="NVim Config"
+	href="https://github.com/LukasPietzschmann/nvim-config" target="_blank"
+	rel="noopener noreferrer">NeoVim config</a>. For example, I once reimplemented GZip
+	decompression in good old C. I also open-source most of my personal work, so check
+	out my <a data-umami-event="GitHub" href="https://github.com/LukasPietzschmann"
+	target="_blank" rel="noopener noreferrer">GitHub</a>! Some of my projects even
+	attracted contributions from others, which I’m way too excited about 😆.
 </p>
 <p>
-	Here's a little collection of projects I enjoy(ed) working on:
+	Since there was no way I could decide on less favorites, here's a selection of
+	six that I really enjoy(ed) working on:
 </p>
 <PersonalProjects projectIds={personalProjects}/>
